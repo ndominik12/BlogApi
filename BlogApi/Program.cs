9b6b3c64d9c4f87a25cf292dadb0d87ad52cb537
ndominik12 <ndomi560@gmail.com>
2026-09-10 12:37:35 +0200

    commit2

--- BlogApi/Program.cs
+++ BlogApi/Program.cs
@@ -1,4 +1,6 @@
 
+using Scalar.AspNetCore;
+
 namespace BlogApi
 {
     public class Program
@@ -19,6 +21,7 @@ namespace BlogApi
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.MapScalarApiReference();
             }
 
             app.UseHttpsRedirection();
