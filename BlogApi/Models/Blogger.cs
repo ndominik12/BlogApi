f652938508e3817f51b7983aae846b1ffa58e359
ndominik12 <ndomi560@gmail.com>
2026-09-17 08:10:55 +0200

    Blogger model is created

---
+++ BlogApi/Models/Blogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+/// <summary>
+/// Summary description for Blogger
+/// </summary>
+/// 
+namespace BlogApi.Models
+{
+    public class Blogger
+{
+	public Blogger()
+	{
+			public int Id { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public int Age { get; set; }
+            public string Password { get; set; }
+            public DateTime RegistrationTime { get; set } 
+
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
